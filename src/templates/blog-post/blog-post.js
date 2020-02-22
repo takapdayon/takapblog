25a39097de0e9e2dba2044153a090fca11504f73
takapdayon <business.takap@gmail.com>
2020-02-22 16:27:20 +0000

    YYYY/MM/DD change

--- src/templates/blog-post/blog-post.js
+++ src/templates/blog-post/blog-post.js
@@ -60,7 +60,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "YYYY/MM/DD")
         description
         hero {
           childImageSharp {
